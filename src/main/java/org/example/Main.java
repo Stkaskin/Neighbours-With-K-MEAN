f1c13951151692b2d523aa529bd8f40d994d8c28
stkaskin <stkaskin01@gmail.com>
2022-10-13 02:35:02 +0300

    dot join

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,15 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Main {
     static int height = 1000;
     static int weight = 1000;
     static BufferedImage img;
+    static Map<point, point> neigmatch = new HashMap<>();
     static ArrayList<point> referancePoints = new ArrayList<>();
 
     class point {
@@ -43,13 +46,18 @@ public class Main {
         KmeanStepNext();
         new Main().findNeighbour();
         new Main().NeigbourColorChange();
+     /*   for (Map.Entry<Main.point, Main.point> pointpointEntry:neigmatch.entrySet())
+        {
+            new Main().WriteDoublePoint(pointpointEntry.getKey(),pointpointEntry.getValue());
+        }*/
+
         new Main().run();
 
         System.out.println("Hello world!");
 
     }
 
-    private void writeSquare(int x, int y, int x1, int y1,Color color) {
+    private void writeSquare(int x, int y, int x1, int y1, Color color) {
         int locx = 0;
         int locx1 = 0;
         int locy = 0;
@@ -84,10 +92,59 @@ public class Main {
 
         }
         for (int i = locy; i < locy1; i++) {
-            img.setRGB(locx1 + 4, i,color.getRGB());
+            img.setRGB(locx1 + 4, i, color.getRGB());
 
         }
 
+
+    }
+
+    private void WriteDoublePoint(point w1, point w2,Color color) {
+
+        double pointerx = w1.x;
+        double pointery = w1.y;
+
+        double x = w1.x - w2.x;
+        int a = x > 0 ? -1 : +1;
+        double y = w2.y - w1.y;
+        double c = y / Math.abs(x);
+
+        for (int i = 0; i < Math.abs(x); i++) {
+            pointerx += a;
+            pointery += c;
+            img.setRGB((int) pointerx, (int) pointery, color.getRGB());
+        }
+
+    }
+
+    private boolean NeigbourColorSwitch(point point, point point_, int temp, int value, Color color, boolean square, boolean pointcross, boolean pointallcross) {
+        if (temp < value) {
+            boolean t = false;
+
+            for (Map.Entry<Main.point, Main.point> pointpointEntry : neigmatch.entrySet()) {
+                if (pointpointEntry.getKey().equals(point)) {
+                    if (pointpointEntry.getValue().equals(point_))
+                        t = true;
+                }
+                if (pointpointEntry.getValue().equals(point)) {
+                    if (pointpointEntry.getKey().equals(point_))
+                        t = true;
+                }
+
+            }
+            if (!t && pointcross) {
+
+                neigmatch.put(point, point_);
+                neigmatch.put(point_, point);
+                new Main().WriteDoublePoint(point, point_,color);
+            }
+            if (pointallcross)
+                new Main().WriteDoublePoint(point, point_,color);
+            if (square)
+                writeSquare(point.x, point.y, point_.x, point_.y, color);
+            return true;
+        } else
+            return false;
 
     }
 
@@ -96,16 +153,12 @@ public class Main {
         for (point point : randompoints) {
             for (point point_ : point.neighbours) {
                 int temp = pisagor(point.x, point.y, point_.x, point_.y);
-                if (temp < 20) {
-                    writeSquare(point.x, point.y, point_.x, point_.y,Color.YELLOW);
+                if (NeigbourColorSwitch(point, point_, temp, 20, Color.YELLOW,true,true,true)) ;
+                else if (
+                        NeigbourColorSwitch(point, point_, temp, 50, Color.GRAY,true,true,true)) ;
+                else if (NeigbourColorSwitch(point, point_, temp, 70, Color.RED,true,true,true)) ;
+                else if (NeigbourColorSwitch(point, point_, temp, 70, Color.green,true,true,true)) ;
 
-                }
-                else if (temp<50)
-                    writeSquare(point.x, point.y, point_.x, point_.y,Color.gray);
-                else if (temp<70)
-                    writeSquare(point.x, point.y, point_.x, point_.y,Color.green);
-                else if (temp<100)
-                    writeSquare(point.x, point.y, point_.x, point_.y,Color.ORANGE);
 
             }
 
